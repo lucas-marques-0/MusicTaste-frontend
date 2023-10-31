4bf71aeebb2694da106cf6d7fa31c4179c075eba
Lucas Marques <lucasmarqueshzx@gmail.com>
2023-10-30 23:25:43 -0300

    corrigindo senhas bd

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -22,9 +22,9 @@ export class LoginComponent {
       this.avisarCamposInvalidos();
     } else {
       const usuarios = await this.loginService.buscarUsuarios();
-      const usuarioEncontrado = usuarios.find((usuario: any) => usuario.username === this.username && usuario.password === this.password);
+      const usuarioEncontrado = usuarios.find((usuario: any) => usuario.username === this.username);
       console.log(usuarioEncontrado)
-      const isAuthenticated = await this.loginService.verifyPassword(this.username.trim(), this.password, usuarioEncontrado[0].password);
+      const isAuthenticated = await this.loginService.verifyPassword(this.username.trim(), this.password, usuarioEncontrado.password);
       console.log(isAuthenticated)
       if (isAuthenticated) {
         this.resetarValores();
