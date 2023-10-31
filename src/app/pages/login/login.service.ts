d1d1e076a72fe1bd99b5b68be4e25d0b7151e317
Lucas Marques <lucasmarqueshzx@gmail.com>
2023-10-31 11:54:30 -0300

    corrigindo senhas bd

--- src/app/pages/login/login.service.ts
+++ src/app/pages/login/login.service.ts
@@ -21,7 +21,7 @@ export class LoginService {
   async verifyPassword(inputUsername: any, inputPassword: any): Promise<boolean> {
     //const usuarios = await this.buscarUsuarios();
     //const usuarioEncontrado = usuarios.find((usuario: any) => usuario.username === inputUsername);
-    const usuarioEncontrado: any = await this.http.post('https://musictaste-backend.onrender.com/usuarios/login', { username: inputUsername, password: inputPassword }).toPromise();
+    const usuarioEncontrado: any = await this.http.get(`https://musictaste-backend.onrender.com/usuarios/login/${inputUsername}`).toPromise();
     const storedPassword = usuarioEncontrado.password;
     const [storedSalt, storedHash] = storedPassword.split(':');
     const salt = CryptoJS.enc.Hex.parse(storedSalt);
