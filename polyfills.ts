8beea19230064114a57ece64986c1f23627666de
Lucas Marques <lucasmarqueshzx@gmail.com>
2023-11-21 11:57:01 -0300

    corrigindo importações

---
+++ polyfills.ts
@@ -0,0 +1,13 @@
+(window as any).global = window;
+
+(self as any).global = self;
+
+import 'core-js/features/array/includes';
+import 'core-js/features/string/includes';
+import 'core-js/features/object/assign';
+import 'core-js/features/object/entries';
+import 'core-js/features/object/values';
+
+import 'crypto-browserify';
+import 'crypto';
+
