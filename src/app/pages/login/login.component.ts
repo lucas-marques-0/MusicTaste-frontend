a60fe8a4e33203978436a9070566d4236912c147
Lucas Marques <lucasmarqueshzx@gmail.com>
2023-11-22 10:22:36 -0300

    testando

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { LoginService } from './login.service';
-
+import * as bcrypt from 'bcryptjs';
+import * as jwt from 'jsonwebtoken';
 
 @Component({
   selector: 'app-login',
@@ -24,12 +25,12 @@ export class LoginComponent {
     } else {
       const usuarioEncontrado: any = this.loginService.verificarUsuarioExistente(this.email);
       if (usuarioEncontrado) {
-        //const senhaCorreta = bcrypt.compareSync(this.password, usuarioEncontrado.password);
-        //if (senhaCorreta) {
-          //this.criarToken(usuarioEncontrado);
-        //} else {
-          //this.avisarLoginIncorreto();
-        //}
+        const senhaCorreta = bcrypt.compareSync(this.password, usuarioEncontrado.password);
+        if (senhaCorreta) {
+          this.criarToken(usuarioEncontrado);
+        } else {
+          this.avisarLoginIncorreto();
+        }
         this.resetarValores();
         this.router.navigate(['/home']);
       } else {
@@ -39,8 +40,8 @@ export class LoginComponent {
   }
 
   criarToken(usuario: any) {
-    //const token = jwt.sign({ id: usuario.id, email: usuario.email }, 'codigo-do-jwt', { expiresIn: '1d' });
-    //return { token, usuario }
+    const token = jwt.sign({ id: usuario.id, email: usuario.email }, 'codigo-do-jwt', { expiresIn: '1d' });
+    return { token, usuario }
   }
  
   verificarLoginUsuario(usuarios: any[], username: string, password: string) {
