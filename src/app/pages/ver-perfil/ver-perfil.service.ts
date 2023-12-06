53478950e13df055267a4a26be4bb1977f2a9183
Lucas Marques <lucasmarqueshzx@gmail.com>
2023-12-06 08:43:57 -0300

    corrigindo erro cors na  rota

--- src/app/pages/ver-perfil/ver-perfil.service.ts
+++ src/app/pages/ver-perfil/ver-perfil.service.ts
@@ -11,12 +11,12 @@ export class VerPerfilService {
 
   async buscarInfosUsuario(userID: any): Promise<any> {
     const token = localStorage.getItem('token');
-    const headersObj = new HttpHeaders({
+    const headers = new HttpHeaders({
       'Authorization': `${token}`,
     });
 
     try {
-      const infosUsuario: any = await this.http.get(`https://musictaste-backend.onrender.com/usuarios/${userID}`, { headers: headersObj }).toPromise();
+      const infosUsuario: any = await this.http.get(`https://musictaste-backend.onrender.com/usuarios/${userID}`, { headers: headers }).toPromise();
       return infosUsuario;
     } catch (error) {
       console.error('Erro ao buscar informações do usuário:', error);
