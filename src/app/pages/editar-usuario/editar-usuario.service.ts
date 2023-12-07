eed17823dacd4d3c0c2f6846685ca760d1f39900
Lucas Marques <lucasmarqueshzx@gmail.com>
2023-12-07 12:46:59 -0300

    corrigindo erro editra-usuario

--- src/app/pages/editar-usuario/editar-usuario.service.ts
+++ src/app/pages/editar-usuario/editar-usuario.service.ts
@@ -1,19 +1,20 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { HomeService } from '../home/home.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EditarUsuarioService {
 
-  constructor(private http: HttpClient, private  homeService: HomeService) { }
+  constructor(private http: HttpClient) { }
 
   async buscarInfosUsuario(userID: any): Promise<any> {
+    const token = localStorage.getItem('token');
     try {
-      return await this.http.get(`https://musictaste-backend.onrender.com/usuarios/${userID}`).toPromise();
+      return await this.http.post(`https://musictaste-backend.onrender.com/usuarios/${userID}`, { token: token }).toPromise();
     } catch (error) {
       console.error('Erro ao buscar informações do usuário:', error);
+      return false;
     }
   }
 
