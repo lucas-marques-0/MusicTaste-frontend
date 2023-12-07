c4aa1e040be74825d1fc0ba8bd315a72d848a929
Lucas Marques <lucasmarqueshzx@gmail.com>
2023-12-07 11:40:54 -0300

    organizando front após resolver header cors

--- src/app/pages/cadastro/cadastro.service.ts
+++ src/app/pages/cadastro/cadastro.service.ts
@@ -11,7 +11,19 @@ export class CadastroService {
 
   async adicionarUsuario(usuario: any, email: any, senha: any): Promise<any> {
     const avatarUsuario = this.avatarUrl + usuario;
-    const listaMusicas = { 0: '', 1: '', 2: '', 3: '', 4: '', 5: '', 6: '', 7: '', 8: '', 9: '' }
+   // const listaMusicas = { 0: '', 1: '', 2: '', 3: '', 4: '', 5: '', 6: '', 7: '', 8: '', 9: '' }
+    let listaMusicas = [
+      { key: '0', value: '' },
+      { key: '1', value: '' },
+      { key: '2', value: '' },
+      { key: '3', value: '' },
+      { key: '4', value: '' },
+      { key: '5', value: '' },
+      { key: '6', value: '' },
+      { key: '7', value: '' },
+      { key: '8', value: '' },
+      { key: '9', value: '' },
+    ]
     try {
       await this.http.post('https://musictaste-backend.onrender.com/usuarios', { username: usuario, email: email, password: senha, avatar: avatarUsuario, musicas: listaMusicas, action: 'cadastro' }).toPromise();
     } catch (error) {
